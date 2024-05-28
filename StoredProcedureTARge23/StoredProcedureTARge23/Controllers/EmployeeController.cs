0dde0b39559b0637e7aa0c888e18c78b4abd0285
Ingvar Derešivski <inde999@gmail.com>
2024-05-28 14:58:34 +0300

    StoredProcedure

--- StoredProcedureTARge23/StoredProcedureTARge23/Controllers/EmployeeController.cs
+++ StoredProcedureTARge23/StoredProcedureTARge23/Controllers/EmployeeController.cs
@@ -63,5 +63,130 @@ namespace StoredProcedureTARge23.Controllers
                 return View(models);
             }
         }
+
+        [HttpPost]
+        public IActionResult DynamicSQL(string firstName, string lastName, string gender, int salary)
+        {
+            string connectionStr = _config.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = con;
+                command.CommandText = "dbo.spSearchEmployees";
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                if (firstName != null)
+                {
+                    SqlParameter param_fn = new SqlParameter("@FirstName", firstName);
+                    command.Parameters.Add(param_fn);
+                }
+                if (lastName != null)
+                {
+                    SqlParameter param_fn = new SqlParameter("@LastName", lastName);
+                    command.Parameters.Add(param_fn);
+                }
+                if (gender != null)
+                {
+                    SqlParameter param_fn = new SqlParameter("@Gender", gender);
+                    command.Parameters.Add(param_fn);
+                }
+                if (salary != 0)
+                {
+                    SqlParameter param_fn = new SqlParameter("@salary", salary);
+                    command.Parameters.Add(param_fn);
+                }
+                con.Open();
+                SqlDataReader sdr = command.ExecuteReader();
+                List<Employee> model = new List<Employee>();
+                while (sdr.Read())
+                {
+                    var details = new Employee();
+                    details.FirstName = sdr["FirstName"].ToString();
+                    details.LastName = sdr["LastName"].ToString();
+                    details.Gender = sdr["Gender"].ToString();
+                    details.Salary = Convert.ToInt32(sdr["Salary"]);
+                    model.Add(details);
+                }
+
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult DynamicSQLInStoredProcedure()
+        {
+            string connectionStr = _config.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "dbo.spSearchEmployeesGoodDynamicSQL";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                List<Employee> models = new List<Employee>();
+                while (sdr.Read())
+                {
+                    var details = new Employee();
+                    details.FirstName = sdr["FirstName"].ToString();
+                    details.LastName = sdr["LastName"].ToString();
+                    details.Gender = sdr["Gender"].ToString();
+                    details.Salary = Convert.ToInt32(sdr["Salary"]);
+                    models.Add(details);
+                }
+
+                return View(models);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult DynamicSQLInStoredProcedure(string firstName, string lastName, string gender, int salary)
+        {
+            string connectionStr = _config.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = con;
+                command.CommandText = "dbo.spSearchEmployeesGoodDynamicSQL";
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                if (firstName != null)
+                {
+                    SqlParameter param_fn = new SqlParameter("@FirstName", firstName);
+                    command.Parameters.Add(param_fn);
+                }
+                if (lastName != null)
+                {
+                    SqlParameter param_fn = new SqlParameter("@LastName", lastName);
+                    command.Parameters.Add(param_fn);
+                }
+                if (gender != null)
+                {
+                    SqlParameter param_fn = new SqlParameter("@Gender", gender);
+                    command.Parameters.Add(param_fn);
+                }
+                if (salary != 0)
+                {
+                    SqlParameter param_fn = new SqlParameter("@salary", salary);
+                    command.Parameters.Add(param_fn);
+                }
+                con.Open();
+                SqlDataReader sdr = command.ExecuteReader();
+                List<Employee> model = new List<Employee>();
+                while (sdr.Read())
+                {
+                    var details = new Employee();
+                    details.FirstName = sdr["FirstName"].ToString();
+                    details.LastName = sdr["LastName"].ToString();
+                    details.Gender = sdr["Gender"].ToString();
+                    details.Salary = Convert.ToInt32(sdr["Salary"]);
+                    model.Add(details);
+                }
+
+                return View(model);
+            }
+        }
     }
 }
